9fb3dba72859a1ccf720b2da978f6cc8e4139598
bblonski <bblonski@localhost>
2010-04-05 06:36:04 +0000

    Removed useless code.

--- Facial Recognition/Launcher.cpp
+++ Facial Recognition/Launcher.cpp
@@ -95,32 +95,6 @@ Launcher::run()
     return 0;
 }
 
-
-void GenerateKey(int vk, BOOL bExtended) {
-
-    KEYBDINPUT  kb = {0};
-    INPUT       Input = {0};
-
-    /* Generate a "key down" */
-    if (bExtended) { kb.dwFlags  = KEYEVENTF_EXTENDEDKEY; }
-    kb.wVk  = vk;
-    Input.type  = INPUT_KEYBOARD;
-    Input.ki  = kb;
-    SendInput(1, &Input, sizeof(Input));
-
-    /* Generate a "key up" */
-    ZeroMemory(&kb, sizeof(KEYBDINPUT));
-    ZeroMemory(&Input, sizeof(INPUT));
-    kb.dwFlags  =  KEYEVENTF_KEYUP;
-    if (bExtended) { kb.dwFlags |= KEYEVENTF_EXTENDEDKEY; }
-    kb.wVk = vk;
-    Input.type = INPUT_KEYBOARD;
-    Input.ki = kb;
-    SendInput(1, &Input, sizeof(Input));
-
-    return;
-}
-
 int main(int argc, char* argv[])
 {
     Launcher* launch = new Launcher();
